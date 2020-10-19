04d5845059b5f9e9ea679eb2c01df245f788625e
San-Dan <70546259+San-Dan@users.noreply.github.com>
2020-10-19 16:17:59 +0200

    basics

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+// This is the file where you can keep your data arrays such as articles and
+// authors.
+// Instructions: In total you should have 5 different authors all with at 
+// least 2 different news feed items each = 1 array per Author and 2 arrays in them?
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// This is the file where you can keep your HTML markup. We should always try to
+// keep us much logic out of the HTML as possible. Put the PHP logic in the top
+// of the files containing HTML or even better; in another PHP file altogether.
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News Page</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
