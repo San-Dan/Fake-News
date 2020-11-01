718014e5ad825d9d61cc939336d0c0bd5c2758bb
San-Dan <70546259+San-Dan@users.noreply.github.com>
2020-11-01 20:37:21 +0100

    arrays

--- data.php
+++ data.php
@@ -4,5 +4,123 @@ declare(strict_types=1);
 
 // This is the file where you can keep your data arrays such as articles and
 // authors.
-// Instructions: In total you should have 5 different authors all with at 
+// Instructions: In total you should have 5 different authors all with at
 // least 2 different news feed items each = 1 array per Author and 2 arrays in them?
+
+$author1 = [
+    'name' => 'Linda Lind',
+    'id' => 1,
+];
+
+$author2 = [
+    'name' => 'hej hejsson',
+    'id' => 2,
+];
+
+$author3 = [
+    'name' => 'blä blägren',
+    'id' => 3,
+];
+
+$author4 = [
+    'name' => 'Sara Silver',
+    'id' => 4,
+];
+
+$author5 = [
+    'name' => 'Lauren Ipsum',
+    'id' => 5,
+];
+
+
+
+$news1 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200706,
+        'likecounter' => 899,
+    ];
+
+
+$news2 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200728,
+        'likecounter' => 33,
+    ];
+
+$news3 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author eller bara skriva ut?',
+        'published-date' => 201016,
+        'like-counter' => 44,
+    ];
+
+$news4 =
+    [
+        'title' => 'Succé för WU20 - trots detta skitår!',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 201102,
+        'likecounter' => 1337,
+    ];
+
+$news5 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200706,
+        'likecounter' => 72,
+    ];
+
+$news6 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200706,
+        'likecounter' => 23,
+    ];
+
+$news7 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200706,
+        'likecounter' => 34,
+    ];
+
+$news8 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200706,
+        'likecounter' => 59,
+    ];
+
+$news9 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200706,
+        'likecounter' => 334,
+    ];
+
+$news10 =
+    [
+        'title' => 'se Evernote',
+        'content' => 'Lorem ipsum...',
+        'author' => 'kunna hämta author, bara skriva ut?, loop?, multidim array?',
+        'published date' => 200706,
+        'likecounter' => 666,
+    ];
