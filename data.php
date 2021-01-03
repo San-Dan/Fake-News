662d58efdc88b5dfa96a5f68ba078a78641ca8cb
San-Dan <70546259+San-Dan@users.noreply.github.com>
2021-01-03 14:47:58 +0100

    changed date format

--- data.php
+++ data.php
@@ -19,7 +19,7 @@ $newsPosts = [
         'alt text' => 'Two women in black dresses, holding eachother',
         'content' => 'The hottest accessory: THE GIRLFRIEND',
         'author' => $authors[0]['name'],
-        'published date' => '20/11/09',
+        'published date' => '09/11/20',
         'likecounter' => 899,
     ],
 
@@ -30,7 +30,7 @@ $newsPosts = [
         'alt text' => 'People in colorful clothes and draped in plastic',
         'content' => 'Just make it weird, no one will see you anyway',
         'author' => $authors[0]['name'],
-        'published date' => '20/10/28',
+        'published date' => '28/10/20',
         'likecounter' => 33,
     ],
 
@@ -41,7 +41,7 @@ $newsPosts = [
         'alt text' => 'Cat sitting, wearing a tux',
         'content' => 'Their talent will make you PURR',
         'author' => $authors[1]['name'],
-        'published date' => '20/10/16',
+        'published date' => '16/10/20',
         'likecounter' => 44,
     ],
 
@@ -52,7 +52,7 @@ $newsPosts = [
         'alt text' => 'Four men dressed in hipster style, or homeless?',
         'content' => 'Has "to make the poor look rich and the rich look poor" gone too far?',
         'author' => $authors[2]['name'],
-        'published date' => '20/10/02',
+        'published date' => '02/10/20',
         'likecounter' => 1337,
     ],
 
@@ -63,7 +63,7 @@ $newsPosts = [
         'alt text' => 'Belt made of top part of a pair of jeans',
         'content' => 'This is hot now and there is nothing you can do about it',
         'author' => $authors[4]['name'],
-        'published date' => '20/09/06',
+        'published date' => '09/09/20',
         'likecounter' => 72,
     ],
 
@@ -74,7 +74,7 @@ $newsPosts = [
         'alt text' => 'Girl in camo patterned dress',
         'content' => '"Don\'t dress to kill, dress to survive"',
         'author' => $authors[2]['name'],
-        'published date' => '20/11/02',
+        'published date' => '16/11/20',
         'likecounter' => 23,
     ],
 
@@ -85,7 +85,7 @@ $newsPosts = [
         'alt text' => 'Cat with purple punk hair',
         'content' => 'Rebel ipsum dolor sit amet, consectetur adipiscing elit',
         'author' => $authors[1]['name'],
-        'published date' => '20/10/16',
+        'published date' => '03/10/20',
         'likecounter' => 34,
     ],
 
@@ -96,7 +96,7 @@ $newsPosts = [
         'alt text' => 'Pink heart saying No Thanks',
         'content' => 'They tend to make everybody look the same',
         'author' => $authors[3]['name'],
-        'published date' => '20/09/26',
+        'published date' => '20/09/20',
         'likecounter' => 59,
     ],
 
@@ -107,7 +107,7 @@ $newsPosts = [
         'alt text' => 'Ginger cat being cute',
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         'author' => $authors[3]['name'],
-        'published date' => '20/07/06',
+        'published date' => '24/07/20',
         'likecounter' => 334,
     ],
 
@@ -117,7 +117,7 @@ $newsPosts = [
         'alt text' => 'Shocked grey cat',
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         'author' => $authors[4]['name'],
-        'published date' => '20/07/06',
+        'published date' => '11/07/20',
         'likecounter' => 666,
     ]
 ];
